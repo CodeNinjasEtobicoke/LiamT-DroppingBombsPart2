d4bb82445cb1467bd578397edcf217fe869cacbf
Stegosaurus1117 <jcarr701@gmail.com>
2025-02-25 18:23:45 -0500

    thehsnfhfbfdkdejrebnfjesjesffg
    jrhijfignigjiiggigigififigergneijnjgkenrjkgb hebgehbgeh bgehbgehjbgehgbehj gker gh kgherhg

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    
+  
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
+        
     }
 
     // Update is called once per frame
@@ -17,6 +23,19 @@ public class GameManager : MonoBehaviour
         if (Input.anyKeyDown)
         {
             spawner.active = true;
+            title.SetActive(false);
+
+        }
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach (GameObject bombObject in nextBomb)
+
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y - 12))
+            {
+
+                Destroy(bombObject);
+            }
+
         }
         
     }
@@ -24,6 +43,8 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds =Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
 
     }
 }
